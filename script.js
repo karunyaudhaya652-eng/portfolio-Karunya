a6f92d39ce6fdf7d39dbc94b59a1f959e0bd713f
Karunya <karunyaudhaya652@gmail.com>
2026-08-01 11:44:45 +0530

    Improved css responsivr

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const popup = document.getElementById("popup");
 const talkBtn = document.querySelector(".talk-btn");
 const closeBtn = document.querySelector(".close");
 
-talkBtn.onclick = () => {
+talkBtn.addEventListener("click", () => {
     popup.style.display = "flex";
-}
+});
 
-closeBtn.onclick = () => {
+closeBtn.addEventListener("click", () => {
     popup.style.display = "none";
-}
+});
 
-window.onclick = (e) => {
-    if (e.target == popup) {
+window.addEventListener("click", (e) => {
+    if (e.target === popup) {
         popup.style.display = "none";
     }
-}
+});
